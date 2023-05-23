faad0c16e8c82111ea057b6c1626cb5f31b28d99
burakTuran00 <burakturan7410@gmail.com>
2023-05-23 13:19:15 +0300

    InfinityPath

---
+++ InfinityPath/Assets/Scripts/Car.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Car : MonoBehaviour
+{
+    public float speed = 5.0f;
+
+    private Vector3 direction;
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        direction.x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        direction.y = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+
+        transform.position += direction;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("BotCar"))
+        {
+            StartCoroutine(Restart());
+        }
+    }
+
+    IEnumerator Restart()
+    {
+        yield return new WaitForSeconds(.25f);
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ InfinityPath/Assets/Scripts/Parallax.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    private MeshRenderer meshRenderer;
+
+    public float waySpeed { get; private set; }
+
+    private void Awake()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        waySpeed = 0.375f;
+    }
+
+    private void Update()
+    {
+        meshRenderer.material.mainTextureOffset +=
+            new Vector2(0f, waySpeed * Time.deltaTime);
+    }
+
+    private void LateUpdate()
+    {
+        IncreaseSpeed();
+    }
+
+    private void IncreaseSpeed()
+    {
+        waySpeed += 0.00001f;
+    }
+}
